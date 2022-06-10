10a4d1eaa595743a944bc0e8b332af65b8dbb4ea
Nikita Matveev <matnik2001@gmail.com>
2022-06-11 01:33:03 +0200

    append parsing scrpits for interfax & kommersant

---
+++ parsing/commerce_parse.py
@@ -0,0 +1,19 @@
+def parse_commerce(url):
+    page = requests.get(url)
+    #set encoding for interfax
+    page.encoding = 'cp65001' 
+    if page.status_code == 200:
+        #init BeautifulSoup
+        soup = BeautifulSoup(page.text, "html.parser")        
+        #taking header
+        title = soup.find('h1', itemprop="headline").text
+        #taking content
+        content = ' '.join(list(map(lambda x : x.text, soup.findAll('p')))) 
+        #taking category
+        category = soup.find('li', class_="crumbs__item").contents[1].text
+        #taking date
+        date = soup.find('time')["datetime"][:10]
+        
+        #return dict with all information
+        return dict(title=title, content=content, category=category, date=date)
+    
---
+++ parsing/interfax_parse.py
@@ -0,0 +1,18 @@
+def parse_interfax(url):
+    page = requests.get(url)
+    #set encoding for interfax
+    page.encoding = 'ptcp154' 
+    if page.status_code == 200:\
+        #init BeautifulSoup
+        soup = BeautifulSoup(page.text, "html.parser")
+        #taking header
+        title = soup.find('h1', itemprop="headline").text
+        #taking content
+        content = ' '.join(list(map(lambda x : x.text, soup.findAll('p')))) 
+        #taking category
+        category = soup.find('aside', class_="textML").contents[1].text
+        #taking date
+        date = soup.find('a',class_="time")["href"][6:].replace('/', '-')
+        
+        #return dict with all information
+        return dict(title=title, content=content, category=category, date=date)
