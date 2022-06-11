869900b63377e70e3cef09dc2861d6ef01b3a94b
Nikita Matveev <matnik2001@gmail.com>
2022-06-11 10:55:47 +0200

    fixed try except

--- parsing/commerce_parse.py
+++ parsing/commerce_parse.py
@@ -1,8 +1,10 @@
 def parse_commerce(url):
-    page = requests.get(url)
-    #set encoding for interfax
-    page.encoding = 'cp65001' 
-    if page.status_code == 200:
+    try:
+        page = requests.get(url)
+        #set encoding for interfax
+        page.encoding = 'cp65001' 
+        if page.status_code != 200:
+            return None
         #init BeautifulSoup
         soup = BeautifulSoup(page.text, "html.parser")        
         #taking header
@@ -13,7 +15,8 @@ def parse_commerce(url):
         category = soup.find('li', class_="crumbs__item").contents[1].text
         #taking date
         date = soup.find('time')["datetime"][:10]
-        
+
         #return dict with all information
         return dict(title=title, content=content, category=category, date=date)
-    
+    except Exception:
+        return None
--- parsing/interfax_parse.py
+++ parsing/interfax_parse.py
@@ -1,8 +1,10 @@
 def parse_interfax(url):
-    page = requests.get(url)
-    #set encoding for interfax
-    page.encoding = 'ptcp154' 
-    if page.status_code == 200:\
+    try:
+        page = requests.get(url)
+        #set encoding for interfax
+        page.encoding = 'ptcp154' 
+        if page.status_code =! 200:
+            return None
         #init BeautifulSoup
         soup = BeautifulSoup(page.text, "html.parser")
         #taking header
@@ -13,6 +15,8 @@ def parse_interfax(url):
         category = soup.find('aside', class_="textML").contents[1].text
         #taking date
         date = soup.find('a',class_="time")["href"][6:].replace('/', '-')
-        
+
         #return dict with all information
         return dict(title=title, content=content, category=category, date=date)
+    except Exception:
+        return None
