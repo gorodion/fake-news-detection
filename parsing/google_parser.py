77ba4599ec45068b08bd2543f11262e4fb565588
Nikita Matveev <matnik2001@gmail.com>
2022-06-11 22:17:19 +0200

    fixed google

--- parsing/google_parser.py
+++ parsing/google_parser.py
@@ -23,12 +23,7 @@ def get_articles_google(query, whitelist, n):
     result_set = {source: [] for source in whitelist.keys()}
     for source, func in whitelist.items():
         for_search = f'inurl:{source} {query}'
-        time.sleep(1)
-        articles = list(search(for_search, stop=n, user_agent="Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"))
-        time.sleep(1)
-        if len(articles) == 0:
-            logging.warning(f'В {source} нет подобных статей')
-        time.sleep(1)
+        articles = search(for_search, stop=n)
         for art in articles:
             news = func(art)
             if news:
@@ -44,7 +39,7 @@ if __name__ == '__main__':
     from pprint import pprint
 
     test_queries = [
-        'Илон Маск запустил ракету'
+        'Купил стартап'
     ]
     for q in test_queries:
         pprint(get_articles_google(q, whitelist, 1))
